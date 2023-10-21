82043ee1a7361c09e8d4811adbca324a8a093868
DCRobledo <daniel.canorobledo@gmail.com>
2023-10-21 11:02:29 +0200

    Implemented WrappedReset function and generalized both the timer handler and delegate to class members

--- Unreal_Engine_Tips/Source/Unreal_Engine_Tips/Lambdas/SelfReset.cpp
+++ Unreal_Engine_Tips/Source/Unreal_Engine_Tips/Lambdas/SelfReset.cpp
@@ -11,34 +11,41 @@ void ASelfReset::BeginPlay()
 	ResetAfterDelay(2.0f);
 }
 
+void ASelfReset::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	_timerDelegate.Unbind();
+}
+
 void ASelfReset::FailCompileResetAfterDelay(float delay)
 {
 	// This doesn't compile because Reset is not an UFUNCTION
-	FTimerHandle timerHandler;
-	// GetWorld()->GetTimerManager().SetTimer(timerHandler, this, &ASelfReset::Reset, delay, false);
+	// GetWorld()->GetTimerManager().SetTimer(_timerHandler, this, &ASelfReset::Reset, delay, false);
 }
 
 void ASelfReset::FailExecuteResetAfterDelay(float delay)
 {
-	// This doesn't execute because the compiler can't find a function named "Reset" withing the delegate's scope
-	FTimerDelegate timerDelegate;
-	timerDelegate.BindUFunction(this, TEXT("Reset"));
-
-	FTimerHandle timerHandler;
-	GetWorld()->GetTimerManager().SetTimer(timerHandler, timerDelegate, delay, false);
+	// This doesn't execute because the compiler can't find a function named "Reset" within the delegate's scope
+	_timerDelegate.BindUFunction(this, TEXT("Reset"));
+	GetWorld()->GetTimerManager().SetTimer(_timerHandler, _timerDelegate, delay, false);
 }
 
 void ASelfReset::ResetAfterDelay(float delay)
 {
 	// We wrap the Reset function within a lambda expression
-	FTimerDelegate timerDelegate;
-	timerDelegate.BindLambda([this, delay]()
+	_timerDelegate.BindLambda([this, delay]()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("I reset after %f seconds!"), delay);
 		Reset();
 	});
+	
+	GetWorld()->GetTimerManager().SetTimer(_timerHandler, _timerDelegate, delay, false);
+}
 
-	FTimerHandle timerHandler;
-	GetWorld()->GetTimerManager().SetTimer(timerHandler, timerDelegate, delay, false);
+void ASelfReset::WrappedReset()
+{
+	UE_LOG(LogTemp, Warning, TEXT("I reset!"));
+	Reset();
 }
 
--- Unreal_Engine_Tips/Source/Unreal_Engine_Tips/Lambdas/SelfReset.h
+++ Unreal_Engine_Tips/Source/Unreal_Engine_Tips/Lambdas/SelfReset.h
@@ -13,9 +13,14 @@ public:
 	ASelfReset();
 
 	virtual void BeginPlay() override;
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
 	
 private:
+	FTimerHandle _timerHandler;
+	FTimerDelegate _timerDelegate;
+	
 	void FailCompileResetAfterDelay(float delay);
 	void FailExecuteResetAfterDelay(float delay);
 	void ResetAfterDelay(float delay);
+	void WrappedReset();
 };
